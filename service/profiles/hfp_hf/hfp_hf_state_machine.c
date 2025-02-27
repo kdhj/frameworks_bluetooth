d69eaef8445bf3c2adef44f8adaebde40e99675f
youhaopan <1440050355@qq.com>
2025-02-27 14:52:29 +0800

    bluetooth: Fix hfp hf loop logic error causing skipped nodes during linked list traversal.

--- service/profiles/hfp_hf/hfp_hf_state_machine.c
+++ service/profiles/hfp_hf/hfp_hf_state_machine.c
@@ -356,18 +356,17 @@ static void hf_service_fake_ciev(hf_state_machine_t* hfsm)
 static void query_current_calls_final(hf_state_machine_t* hfsm)
 {
     BT_LOGD("Query current call final");
-    bt_list_node_t *unode;
+    bt_list_node_t* unode;
     bt_list_t* clist = hfsm->current_calls;
     bt_list_t* ulist = hfsm->update_calls;
-    bt_list_node_t *cnode = bt_list_head(clist);
+    bt_list_node_t* cnode = bt_list_head(clist);
 
     hf_service_fake_ciev(hfsm);
 
-    while(cnode)
-    {
+    while(cnode) {
         hfp_current_call_t* ccall = bt_list_node(cnode);
         hfp_current_call_t* ucall = bt_list_find(ulist, call_index_cmp, &ccall->index);
-        bt_list_node_t *next_node = bt_list_next(clist, cnode);
+        bt_list_node_t* next_node = bt_list_next(clist, cnode);
         if (!ucall) {
             /* call not found from update list, notify had terminated */
             ccall->state = HFP_HF_CALL_STATE_DISCONNECTED;
